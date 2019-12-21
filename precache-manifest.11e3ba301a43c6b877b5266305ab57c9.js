6853ae2d4d8a1a6074a1b85f9801bf3f4bbb8f87
Yuk <yuk@Yukde-MacBook-Air.local>
2019-12-21 10:34:16 +0800

    production build

---
+++ precache-manifest.11e3ba301a43c6b877b5266305ab57c9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de13319e0653da551d07bc3a2fcb75a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e68466d1c5ae512e151e",
+    "url": "/static/css/main.e5a49db0.chunk.css"
+  },
+  {
+    "revision": "33a9697cb252d8e2d042",
+    "url": "/static/js/2.a9d1b10e.chunk.js"
+  },
+  {
+    "revision": "e817fa0d151073cffb63aea0e7eba91a",
+    "url": "/static/js/2.a9d1b10e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e68466d1c5ae512e151e",
+    "url": "/static/js/main.c9301164.chunk.js"
+  },
+  {
+    "revision": "5a93544e3b3177289d69",
+    "url": "/static/js/runtime-main.d202421e.js"
+  },
+  {
+    "revision": "b1abb878e2529cb5cb4450139844155d",
+    "url": "/static/media/AvenirLTStd-Black.b1abb878.otf"
+  },
+  {
+    "revision": "ecb0c2ae369ba2a89d9a1ec2a1b3187b",
+    "url": "/static/media/AvenirLTStd-Book.ecb0c2ae.otf"
+  },
+  {
+    "revision": "a7edaaca7240679d0cda0cce2c2e896e",
+    "url": "/static/media/AvenirLTStd-Heavy.a7edaaca.otf"
+  },
+  {
+    "revision": "0b78ec9d509f67bfe3f8458c9d285df0",
+    "url": "/static/media/AvenirLTStd-Light.0b78ec9d.otf"
+  },
+  {
+    "revision": "4f995fa49446998983e05df9994dc96c",
+    "url": "/static/media/AvenirLTStd-Medium.4f995fa4.otf"
+  },
+  {
+    "revision": "e5acdbb63604b7d94aca525e4f99d083",
+    "url": "/static/media/Mother.e5acdbb6.jpg"
+  },
+  {
+    "revision": "c61fb1ded9e5f1c8102ca20a84083f0d",
+    "url": "/static/media/angle-right-solid.c61fb1de.svg"
+  },
+  {
+    "revision": "bf31b2f444463c131dbf2985c2ba4ef4",
+    "url": "/static/media/contact-bg.bf31b2f4.jpg"
+  },
+  {
+    "revision": "d8579c86f036e998d984546cac3e4926",
+    "url": "/static/media/news-bg.d8579c86.jpg"
+  },
+  {
+    "revision": "83bb9b89d3d803e356578202ceefb23c",
+    "url": "/static/media/pandaTV.83bb9b89.mp4"
+  },
+  {
+    "revision": "81707f3577345dd3320fa4e798fad329",
+    "url": "/static/media/paul-gilmore-iMtJT_NEWp8-unsplash.81707f35.jpg"
+  },
+  {
+    "revision": "dbc07244e636e54c4124026744e9c346",
+    "url": "/static/media/radio-bg.dbc07244.jpg"
+  },
+  {
+    "revision": "c942f3982b80831896a7d60c5fc81532",
+    "url": "/static/media/ukpandatvlogo.c942f398.png"
+  },
+  {
+    "revision": "41fee76c63d36d8ae6cf0054c42b53e9",
+    "url": "/static/media/video-bg.41fee76c.jpg"
+  }
+]);
